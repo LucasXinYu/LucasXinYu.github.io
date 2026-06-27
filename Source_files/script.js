736d1059927bef949b134b04b0cda6ef447c6f3b
LucasXinYu <xmy5152@psu.edu>
2026-06-27 16:06:48 -0700

    Update homepage papers, links, and visitor map fallback

--- Source_files/script.js
+++ Source_files/script.js
@@ -1,1 +1,36 @@
-$(function(){$('[data-toggle="popover"]').popover({html:true,content:function(){return '<img class="img-fluid" width="100px" src="'+$(this).data('img')+'" />';}})})
+$(function(){
+  $('[data-toggle="popover"]').popover({
+    html:true,
+    content:function(){
+      return '<img class="img-fluid" width="100px" src="'+$(this).data('img')+'" />';
+    }
+  });
+
+  var $embed = $('.visitor-widget-embed');
+  var $fallback = $('.visitor-map-fallback');
+
+  if (!$embed.length || !$fallback.length) {
+    return;
+  }
+
+  function showVisitorMapFallback() {
+    $fallback.show();
+    $('#mapmyvisitors').hide();
+    $('#mapmyvisitors-widget').hide();
+  }
+
+  if (window.location.protocol === 'file:') {
+    showVisitorMapFallback();
+    return;
+  }
+
+  window.setTimeout(function() {
+    var $widget = $('#mapmyvisitors-widget');
+    var widgetText = ($widget.text() || '').trim().toLowerCase();
+    var isStuckLoading = !$widget.length || widgetText.indexOf('loading data') !== -1;
+
+    if (isStuckLoading) {
+      showVisitorMapFallback();
+    }
+  }, 5000);
+});
